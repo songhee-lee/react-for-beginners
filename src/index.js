20f20f9e6a425b0c0e64a92072aebee42d644f96
songhee <songhee172@gmail.com>
2022-05-30 15:41:29 +0900

    7. Movie App setting

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Home />
   </React.StrictMode>,
   document.getElementById("root")
 );
